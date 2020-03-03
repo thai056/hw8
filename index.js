4cab0b7f3d911366d1a2217b318ba2d433d6db8f
tanakorn0314 <tanakorn0412@gmail.com>
2020-03-03 23:44:48 +0700

    add img field

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use('/api', bodyParser.json(), router);   //[use json]
 app.use('/api', bodyParser.urlencoded({ extended: false }), router);
 
 let bears = [
-    { 'id': 0, 'name': 'pooh', 'weight': 211 },
-    { 'id': 1, 'name': 'vinnie', 'weight': 111 }
+    { 'id': 0, 'name': 'pooh', 'weight': 211, 'img': 'https://lumiere-a.akamaihd.net/v1/images/c94eed56a5e84479a2939c9172434567c0147d4f.jpeg?region=0,0,600,600' },
+    { 'id': 1, 'name': 'vinnie', 'weight': 111, 'img': 'https://cdn2.mhpbooks.com/2018/07/winnie_pooh_PNG37592.png' }
 ];
 
 router.route('/bears')
@@ -23,6 +23,7 @@ router.route('/bears')
         bear.id = bears[bears.length - 1].id + 1;
         bear.name = req.body.name
         bear.weight = req.body.weight
+        bear.img = req.body.img
         bears.push(bear);
         res.json({ message: 'Bear created!' })
     })
@@ -38,6 +39,7 @@ router.route('/bears/:bear_id')
         let index = bears.findIndex(bear => (bear.id === +id))
         bears[index].name = req.body.name;
         bears[index].weight = req.body.weight;
+        bears[index].img = req.body.img;
         res.json({ message: 'Bear updated!' + req.params.bear_id });
     })
     .delete((req, res) => {                   // Delete a bear
