2db29def81e199e3f5ace1e3bc9c94a0699ce90c
thai056 <jearananthai@gmail.com>
2020-03-30 00:58:02 +0700

    commited

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let router = express.Router();
 let cors = require('cors');
+var session = require('express-session')
 let app = express();
-app.use(cors());
-
+const FB = require('./fb')
+const env = require('dotenv').config()
+var request = require('request');
+const authRoutes = require('./routes/auth')
+const fbRoutes = require('./routes/fb')
+const psuRoutes = require('./routes/psu')
+app.use(cors({ origin: ['http://localhost:3000'], methods: ['GET', 'POST', 'PUT','DELETE'], credentials: true }));
+/* session*/
+app.use(session({
+    secret: 'keyboard cat', cookie: { maxAge: 60000 },
+    resave: false, saveUninitialized: false
+}))
 // all of our routes will be prefixed with /api
 app.use('/api', bodyParser.json(), router);   //[use json]
 app.use('/api', bodyParser.urlencoded({ extended: false }), router);
-
 let bears = [
     { 'id': 0, 'name': 'pooh', 'weight': 211, 'img': 'https://lumiere-a.akamaihd.net/v1/images/c94eed56a5e84479a2939c9172434567c0147d4f.jpeg?region=0,0,600,600' },
     { 'id': 1, 'name': 'vinnie', 'weight': 111, 'img': 'https://cdn2.mhpbooks.com/2018/07/winnie_pooh_PNG37592.png' },
-    { 'id': 2, 'name': 'test', weight: 999, img: 'http://test.com'}
 ];
 
 router.route('/bears')
@@ -28,7 +37,6 @@ router.route('/bears')
         bears.push(bear);
         res.json({ message: 'Bear created!' })
     })
-
 router.route('/bears/:bear_id')
     .get((req, res) => {
         let id = req.params.bear_id
@@ -51,6 +59,20 @@ router.route('/bears/:bear_id')
         res.json({ message: 'Bear deleted: ' + req.params.bear_id });
     })
 
+/* face book*/
+router.route('/auth')
+    .get(authRoutes.index);
+router.route('/auth/logout')
+    .get(authRoutes.logout);
+router.route('/auth/facebook')
+    .get(fbRoutes.loginUrl);
+router.route('/auth/facebook/login/callback')
+    .get(fbRoutes.loginCallback);
+
+/* PSU*/
+router.route('/auth/psu')
+    .post(psuRoutes.login);
 
 app.use("*", (req, res) => res.status(404).send('404 Not found'));
-app.listen(3000, () => console.log("Server is running"));
+//app.listen(process.env.PORT, () => console.log(process.env.PORT));
+app.listen(80, () => console.log("Server is running"));
